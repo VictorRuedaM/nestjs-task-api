c062fc48afe28e1e16d497742d5074e23d47a006
Victor Rueda <vialruma1985@gmail.com>
2023-06-23 17:56:14 -0500

    HttpStatus and controllers were created

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from 'src/dto/create-task.dto';
+import { UpdateTaskDto } from 'src/dto/update-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+  @Get(':id')
+  getOneTask(@Param('id') id: string) {
+    return this.taskService.getOneTask(id);
+  }
+  @Get()
+  getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+  @Post()
+  createTask(@Body() task: CreateTaskDto) {
+    return this.taskService.createTask(task);
+  }
+  @Patch(':id')
+  updateTask(@Param('id') id: string, @Body() task: UpdateTaskDto) {
+    return this.taskService.updateTask(id, task);
+  }
+  @Delete(':id')
+  @HttpCode(204)
+  deleteTask(@Param('id') id: string) {
+    return this.taskService.deleteTask(id);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateTaskDto } from 'src/dto/create-task.dto';
@@ -10,26 +15,66 @@ export class TasksService {
   constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
 
   async getOneTask(id: string) {
-    const taskFound = await this.taskModel.findById(id);
-    return taskFound;
+    try {
+      const taskFound = await this.taskModel.findById(id);
+      if (!taskFound) {
+        throw new HttpException('Tasks Not Found In DB', HttpStatus.NOT_FOUND);
+      }
+      return taskFound;
+    } catch (error) {
+      throw error;
+    }
   }
-  async getAllTask() {
-    return this.taskModel.find();
+  async getAllTasks() {
+    try {
+      const tasksFound = await this.taskModel.find();
+      if (!tasksFound.length) {
+        throw new HttpException('Tasks Not Found In DB', HttpStatus.NOT_FOUND);
+      }
+      return tasksFound;
+    } catch (error) {
+      throw error;
+    }
   }
 
   async createTask(createTask: CreateTaskDto) {
-    const newTask = await new this.taskModel(createTask);
-    return newTask.save();
+    try {
+      const newTask = new this.taskModel(createTask);
+      return await newTask.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Task Already Exists');
+      }
+      throw error;
+    }
   }
 
   async updateTask(id: string, updateTask: UpdateTaskDto) {
-    const taskUpdated = await this.taskModel.findByIdAndUpdate(id, updateTask, {
-      new: true,
-    });
-    return taskUpdated;
+    try {
+      const taskFound = await this.taskModel.findById(id);
+      if (!taskFound) {
+        throw new HttpException('Task Not Found', HttpStatus.NOT_FOUND);
+      }
+      const taskUpdated = await this.taskModel.findByIdAndUpdate(
+        id,
+        updateTask,
+        {
+          new: true,
+        },
+      );
+      return taskUpdated;
+    } catch (error) {
+      throw error;
+    }
   }
   async deleteTask(id: string) {
-    const taskDeleted = await this.taskModel.findByIdAndDelete(id);
-    return taskDeleted;
+    try {
+      const taskDeleted = await this.taskModel.findByIdAndDelete(id);
+      if (!taskDeleted) {
+        throw new HttpException('Task Not Found', HttpStatus.NOT_FOUND);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 }
