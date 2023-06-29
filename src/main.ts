3cea72e989d0ed0777f0b011a00de491b366244c
Victor Rueda <vialruma1985@gmail.com>
2023-06-28 19:45:42 -0500

    Cors config and prefix api added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+const whiteList = process.env.CLIENT_URL;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: whiteList });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
